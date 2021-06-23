bae9fb57fb87ff8819558fa9c6276643bee3df23
amarasanoh <sanohawa@gmail.com>
2021-06-23 12:15:27 +0200

    Fix notice board

--- src/containers/NoticeBoard.js
+++ src/containers/NoticeBoard.js
@@ -9,12 +9,12 @@ const NoticeBoard = ({calculatorInfos}) => {
         <div className='row m-2'>
             <div className='col-xs-12 col-sm-12 offset-md-4 col-md-4 offset-lg-4 col-lg-4 border border-success'>
                 <div className='p-2'>
-                    <div className='row' style={{height: 10}}>
+                    <div className='row mb-2' style={{height: 10}}>
                         <div className='col-12 text-end'>
                             {calculatorInfos.expression+' ='}
                         </div>
                     </div>
-                    <div className='row p-2' style={{height: 45}}>
+                    <div className='row' style={{height: 33}}>
                         <div className='col-12 text-end fs-4 fw-bold'>
                             {calculatorInfos.compute}
                         </div>
